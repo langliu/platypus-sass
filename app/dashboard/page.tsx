25b9bd1c1c05f468dcfb42f84bb685f26fdc6a2b
langliu <langliu1216@gmail.com>
2024-05-19 23:58:29 +0800

    feat: ✨ 添加左右布局

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { ModeToggle } from '@/components/mode-toggle'
+export default function DashboardPage() {
+  return (
+    <div>
+      Dashboard Page
+      <ModeToggle />
+    </div>
+  )
+}
