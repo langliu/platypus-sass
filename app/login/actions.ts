0a2dda0eb60b00001c4560f542c23e0a648048bb
langliu <langliu1216@gmail.com>
2024-05-22 22:42:05 +0800

    feat(login): ✨ 完善登录页布局

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -2,19 +2,14 @@
 
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
-import { headers } from 'next/headers'
 import { createClient } from '@/lib/supabase/server'
 
 export async function login(formData: FormData) {
   const supabase = createClient()
-  const origin = headers().get('origin')
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
   const data = {
     email: formData.get('email') as string,
     password: formData.get('password') as string,
   }
-  console.log('lasdas', data)
   const { error } = await supabase.auth.signInWithPassword(data)
 
   if (error) {
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,91 +1,96 @@
-import { Button } from '@/components/ui/button'
-// import { useTheme } from 'next-themes'
-import { headers } from 'next/headers'
-import { login, signup } from './actions'
-import { createClient } from '@/lib/supabase/server'
-import { redirect } from 'next/navigation'
+import type { Metadata } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
 
-export default function LoginPage() {
-  // const supabase = createClient()
-  const origin = headers().get('origin')
-  // const origin = window.origin
-  // const { theme } = useTheme()
-  console.log('them', origin)
+import { cn } from '@/lib/utils'
+import { buttonVariants } from '@/components/ui/button'
+import { UserAuthForm } from './(components)/user-auth-form'
 
-  const signIn = async () => {
-    'use server'
+export const metadata: Metadata = {
+  title: 'Authentication',
+  description: 'Authentication forms built using the components.',
+}
 
-    // 1. Create a Supabase client
-    const supabase = createClient()
-    const origin = headers().get('origin')
-    // 2. Sign in with GitHub
-    const { error, data } = await supabase.auth.signInWithOAuth({
-      provider: 'github',
-      options: {
-        redirectTo: `${origin}/auth/callback`,
-      },
-    })
-
-    if (error) {
-      console.log(error)
-    } else {
-      return redirect(data.url)
-    }
-    // 3. Redirect to landing page
-  }
-
+export default function AuthenticationPage() {
   return (
-    <form className='flex flex-col gap-4'>
-      <div>
-        <label htmlFor='email'>Email:</label>
-        <input id='email' name='email' type='email' required />
+    <>
+      <div className='md:hidden'>
+        <Image
+          src='/examples/authentication-light.png'
+          width={1280}
+          height={843}
+          alt='Authentication'
+          className='block dark:hidden'
+        />
+        <Image
+          src='/examples/authentication-dark.png'
+          width={1280}
+          height={843}
+          alt='Authentication'
+          className='hidden dark:block'
+        />
       </div>
-      <div>
-        <label htmlFor='password'>Password:</label>
-        <input id='password' name='password' type='password' required />
+      <div className='container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0'>
+        <Link
+          href='/examples/authentication'
+          className={cn(
+            buttonVariants({ variant: 'ghost' }),
+            'absolute right-4 top-4 md:right-8 md:top-8',
+          )}
+        >
+          注册
+        </Link>
+        <div className='relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r'>
+          <div className='absolute inset-0 bg-zinc-900' />
+          <div className='relative z-20 flex items-center text-lg font-medium'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='currentColor'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              className='mr-2 h-6 w-6'
+            >
+              <title>Acme Inc</title>
+              <path d='M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3' />
+            </svg>
+            Platypus Sass
+          </div>
+          <div className='relative z-20 mt-auto'>
+            <blockquote className='space-y-2'>
+              <p className='text-lg'>
+                &ldquo;This library has saved me countless hours of work and helped me deliver
+                stunning designs to my clients faster than ever before.&rdquo;
+              </p>
+              <footer className='text-sm'>Sofia Davis</footer>
+            </blockquote>
+          </div>
+        </div>
+        <div className='lg:p-8'>
+          <div className='mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]'>
+            <div className='flex flex-col space-y-2 text-center'>
+              <h1 className='text-2xl font-semibold tracking-tight'>登录</h1>
+              <p className='text-sm text-muted-foreground'>
+                在下方输入你的账号密码进行登录
+              </p>
+            </div>
+            <UserAuthForm />
+            <p className='px-8 text-center text-sm text-muted-foreground'>
+              By clicking continue, you agree to our{' '}
+              <Link href='/terms' className='underline underline-offset-4 hover:text-primary'>
+                Terms of Service
+              </Link>{' '}
+              and{' '}
+              <Link href='/privacy' className='underline underline-offset-4 hover:text-primary'>
+                Privacy Policy
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
       </div>
-      <div className='flex gap-4'>
-        <Button formAction={login}>Log in</Button>
-        <Button formAction={signup}>Sign up</Button>
-      </div>
-    </form>
-    // <div className='w-[400px] border border-gray-200 rounded-2xl p-8'>
-    //   <h1 className='text-center text-2xl font-bold'>Platypus Sass</h1>
-    //   <Auth
-    //     supabaseClient={supabase}
-    //     appearance={{
-    //       theme: ThemeSupa,
-    //     }}
-    //     localization={{
-    //       variables: {
-    //         sign_in: {
-    //           email_label: '邮件地址',
-    //           email_input_placeholder: '请输入你的邮件地址',
-    //           password_label: '登录密码',
-    //           password_input_placeholder: '请输入你的登录密码',
-    //           button_label: '登录',
-    //           link_text: '已有账号？立即登录',
-    //         },
-    //         sign_up: {
-    //           link_text: '没有账号？立即注册',
-    //           email_label: '邮件地址',
-    //           email_input_placeholder: '请输入你的邮件地址',
-    //           password_label: '登录密码',
-    //           password_input_placeholder: '请输入你的登录密码',
-    //           button_label: '注册',
-    //           confirmation_text: '查看您邮件里的确认链接',
-    //         },
-    //         forgotten_password: {
-    //           link_text: '忘记密码？',
-    //           email_label: '邮件地址',
-    //           email_input_placeholder: '请输入你的邮件地址',
-    //           button_label: '发送重置密码说明',
-    //         },
-    //       },
-    //     }}
-    //     redirectTo={`${origin}/auth/callback`}
-    //     theme={'dark'}
-    //   />
-    // </div>
+    </>
   )
 }
